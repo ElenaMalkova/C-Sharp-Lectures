f0d4ad8e55869a73113effc747a997cfae67aae6
Elena <malkovva@gmail.com>
2022-07-08 22:51:47 +0300

    add Example 003 Sum of two numbers

---
+++ Example003_HelloSomebody/Program.cs
@@ -0,0 +1,7 @@
+﻿// Написать программу, в которую вводятся два числа и получается сумма
+Console.WriteLine ("Введите целое положительное число");
+int numberA = int.Parse(Console.ReadLine ());
+Console.WriteLine ("Введите целое положительное число");
+int numberB = int.Parse(Console.ReadLine ());
+int result = numberA + numberB;
+Console.WriteLine ("Cумма =" + result);
