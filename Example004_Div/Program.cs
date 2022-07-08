9af04f4c0d3d37067bbf3e23111d89cd9fdc2fb4
Elena <malkovva@gmail.com>
2022-07-08 23:37:54 +0300

    add Example 5 Summing random numbers

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, в которую вводятся два числа и находится частное этих чисел
+Console.WriteLine ("Введите положительное число");
+double numberA = int.Parse(Console.ReadLine ());
+Console.WriteLine ("Введите положительное число");
+double numberB = int.Parse(Console.ReadLine ());
+double result = numberA / numberB;
+Console.WriteLine ("Частное =" + result);
---
+++ Example005_Random Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿// Написать программу, в которую вводятся два числа и получается сумма
+int numberA = new Random().Next(1,10); //1 2 3 4 ...9
+Console.WriteLine( numberA );
+int numberB = new Random().Next(1,10);
+Console.WriteLine( numberB );
+int result = numberA + numberB;
+Console.WriteLine ("Cумма=" + result);
