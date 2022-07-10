7b555e2f82ed0a47b5cbedea74a483e3e8b37a36
Elena <malkovva@gmail.com>
2022-07-10 11:54:45 +0300

    add Example Max Number

---
+++ Example007_MaxNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// Находим максимальное число из заданных
+int a = 9;
+int b = 8;
+int c = 1;
+int d = 4;
+int e = 6;
+int max = a;
+if (b > max) max = b;
+if ( c> max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write ("max = ");
+Console.WriteLine (max);
